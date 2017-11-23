62a6b99642d42d05a1116fe02da78cdf85e15a70
szczypiorofix <szczypiorofix@o2.pl>
2017-11-23 14:44:31 +0100

    Socket.io chat - początek projektu.

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(8000, function(){
+  console.log('listening on *:8000');
+});
