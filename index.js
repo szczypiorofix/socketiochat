68a2058e43a59eb561ac4d1a8d7744bb5f36d7ca
szczypiorofix <szczypiorofix@o2.pl>
2017-11-30 16:06:38 +0100

    Dodanie ksywki w czacie. Poprawki w wyglądzie. Dodanie okna konsoli.

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 80;
+
+
+// USING STATIC CSS & JS FILEs
+app.use(express.static('css'));
+app.use(express.static('js'));
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket){  
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+  socket.on('chat name', function(name){
+    io.emit('chat name', name);
   });
 });
 
---
+++ js/mainscript.js
@@ -0,0 +1,31 @@
+
+var user_name = prompt("Please enter your name");
+if (user_name == null || user_name == '') user_name = "Todd Howard";
+
+$('#message_name').val(user_name);
+$('#message_input').focus();
+
+$(function () {
+  var socket = io();
+  $('form').submit(function() {
+    socket.emit('chat name', $('#message_name').val());
+    socket.emit('chat message', $('#message_input').val());
+    $('#message_input').val('');
+    return false;
+  });
+
+  socket.on('chat name', function(name) {
+    $('#names').append($('<li>').text(name));
+    $('#console-p').append(name+" is typing: ");
+  });
+
+  socket.on('chat message', function(msg) {
+    $('#messages').append($('<li>').text(msg));
+    $('#console-p').append(msg+"<br>");
+    $('#console-p').scrollTop($('#console-p')[0].scrollHeight);
+    $('#messages-div-id').scrollTop($('#messages-div-id')[0].scrollHeight);
+  });
+
+  
+  
+});
