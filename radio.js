ecccaaac0565af13c92229e618859494fec99443
szczypiorofix <szczypiorofix@o2.pl>
2017-12-05 16:26:04 +0100

    Streaming christmas music!! YEAH!

---
+++ radio.js
@@ -0,0 +1,13 @@
+var http = require('http'),
+fs   = require('fs'),
+filePath = 'jingle.mp3',
+stat = fs.statSync(filePath);
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
+    fs.createReadStream(filePath).pipe(response);
+})
+.listen(9000);
