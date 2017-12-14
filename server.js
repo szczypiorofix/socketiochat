837db9588b138abb9ec6255d58d0ab22121b7d82
szczypiorofix <Scypiur1>
2017-12-14 19:28:22 +0100

    Zamienino 2 serwery na 1 do obsługi zarówno strony jak i muzyki

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var fs = require('fs');
 var favicon = require('serve-favicon');
 var mymodule = require('./mymodule');
+
+var songName1 = 'song1.mp3';
+var songFile1 = fs.statSync(songName1);
+
+var songName2 = 'song2.mp3';
+var songFile2 = fs.statSync(songName2);
+
+var songName3 = 'song3.mp3';
+var songFile3 = fs.statSync(songName3);
+
+var songName4 = 'song4.mp3';
+var songFile4 = fs.statSync(songName4);
+
+var songName5 = 'song5.mp3';
+var songFile5 = fs.statSync(songName5);
 
 var user_sockets = new Array();
 var users = new Array();
@@ -48,6 +64,45 @@ app.use(favicon(path.join(__dirname, 'public', 'icon.png')))
 app.get('/?', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
+
+
+// MUSICBOX
+app.get('/musicbox/song1', function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': songFile1.size
+    });
+    fs.createReadStream(songName1).pipe(response);
+});
+app.get('/musicbox/song2', function(request, response) {
+  response.writeHead(200, {
+      'Content-Type': 'audio/mpeg',
+      'Content-Length': songFile2.size
+  });
+  fs.createReadStream(songName2).pipe(response);
+});
+app.get('/musicbox/song3', function(request, response) {
+  response.writeHead(200, {
+      'Content-Type': 'audio/mpeg',
+      'Content-Length': songFile3.size
+  });
+  fs.createReadStream(songName3).pipe(response);
+});
+app.get('/musicbox/song4', function(request, response) {
+  response.writeHead(200, {
+      'Content-Type': 'audio/mpeg',
+      'Content-Length': songFile4.size
+  });
+  fs.createReadStream(songName4).pipe(response);
+});
+app.get('/musicbox/song5', function(request, response) {
+  response.writeHead(200, {
+      'Content-Type': 'audio/mpeg',
+      'Content-Length': songFile5.size
+  });
+  fs.createReadStream(songName5).pipe(response);
+});
+
 
 
 var onConnect = function(socket) {
